b885f3dc4eb6319614afffab26c6e2a9f01c13a4
Pedro Domingues Santos <pedrosanttzz2012@gmail.com>
2025-12-16 14:01:23 -0300

    Criação de fragmentos de elementos DOOM no componente
    O professor ensinou a escrever fragmentos de eementos DOOM no componente utilizando tanto "React.Fragment" quanto só os sinais "<>...</>". A diferença é que para utilizar o "React.Fragment" precisa-se importar no arquivo a biblioteca do React. Agora para utilizar os simples sinais "<>...</>", não é necessária a importação.

---
+++ meu-app/src/app/basico/fragment1/page.jsx
@@ -0,0 +1,22 @@
+
+import React from "react";
+
+
+export default function Fragment1() {
+    
+    return (
+        <React.Fragment>
+            <div className="flex flex-col justify-center items-center">
+                <div className="w-8/10 p-10">
+                    <h1 className="text-3xl font-bold text-center">Fragment1</h1>
+                </div>
+            </div>
+
+            <div className="flex flex-col justify-center items-center">
+                <div className="w-8/10 p-10">
+                    <h1 className="text-3xl font-bold text-center">Fragment1</h1>
+                </div>
+            </div>
+        </React.Fragment>
+  );
+}
---
+++ meu-app/src/app/basico/fragment2/page.jsx
@@ -0,0 +1,19 @@
+
+export default function Fragment2() {
+    
+    return (
+        <>
+            <div className="flex flex-col justify-center items-center">
+                <div className="w-8/10 p-10">
+                    <h1 className="text-3xl font-bold text-center">Fragment1</h1>
+                </div>
+            </div>
+
+            <div className="flex flex-col justify-center items-center">
+                <div className="w-8/10 p-10">
+                    <h1 className="text-3xl font-bold text-center">Fragment1</h1>
+                </div>
+            </div>
+        </>
+  );
+}
